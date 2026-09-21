b3f871642968752846eb10ef2b03213ba6c4481f
zxsla <jierryx9@gmail.com>
2026-09-21 15:10:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,141 @@
 var config = {
+  "spider": "https://tc-new.z.wiki/autoupload/k2fxc/20260902/xJZz/fish09020618.jar;md5;282aee405654188f1bc1822bbf137410",
+  "logo": "https://modelscope-resouces.oss-cn-zhangjiakou.aliyuncs.com/avatar%2Ff5309172-1af6-4c4f-a1c5-a4773a1cc718.gif",
+  "wallpaper": "https://gradient-image-generator.ituyama01.workers.dev/gradient?w=1920&h=1080&seed=random&warp=0.8&grain=0.1",
+  "filter": {},
+  "ads": [
+    "agad.cn",
+    "adu.men",
+    "pushtool.duapp.com"
+  ],
   "sites": [
     {
+      "key": "Duboku",
+      "name": "独播影视",
+      "type": 3,
+      "api": "csp_Duboku",
+      "searchable": 1,
+      "filterable": 1,
+      "changeable": 1,
+      "categories": [
+        "华语电影",
+        "高分剧集",
+        "恐怖片",
+        "动作片",
+        "短剧",
+        "综艺",
+        "音乐"
+      ]
+    },
+    {
       "key": "maoyanzy",
-      "name": "测试源",
+      "name": "Eternity资源",
       "type": 1,
       "api": "https://www.maoyanzy.com/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "Guazi",
+      "name": "Eternity影视",
+      "type": 3,
+      "api": "csp_Guazi",
       "searchable": 1
+    },
+    {
+      "key": "aiting",
+      "name": "Eternity音乐",
+      "type": 3,
+      "api": "csp_AiTingMusic",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 0
+    },
+    {
+      "key": "TingShijie",
+      "name": "Eternity听书",
+      "type": 3,
+      "api": "csp_TingShijie",
+      "searchable": 1,
+      "style": {
+        "type": "list"
+      }
+    },
+    {
+      "key": "feiye",
+      "name": "绯夜",
+      "type": 1,
+      "api": "https://jierry123.eu.cc/api/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "jisuzy",
+      "name": "二宝子",
+      "type": 1,
+      "api": "https://www.jisuzy.com/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "tyyszy",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "https://tyyszy.com/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "ffzy",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "http://www.ffzy.tv/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "lzzy",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "https://www.lzzy.tv/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "niuniuzy",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "https://niuniuzy6.com/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "shandianzy",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "https://shandianzy.com/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
+    },
+    {
+      "key": "okzyw",
+      "name": "皆可资源",
+      "type": 1,
+      "api": "https://okzyw.cc/api.php/provide/vod/",
+      "searchable": 1,
+      "quickSearch": 1,
+      "filterable": 1
     }
-  ]
+  ],
+  "lives": [],
+  "parses": []
 };
-config;
+module.exports = config;
